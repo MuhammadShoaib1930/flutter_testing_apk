6904b217d244d8cda4ac690c9f28aa49f88ad55d
M.shoaib <shoaibsn54321@gmail.com>
2024-08-16 09:25:18 +0500

    changing text

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Muhammad Shoaib',style: TextStyle(color: Color.fromARGB(255, 94, 88, 88)),
             ),
             Text(
               '$_counter',
